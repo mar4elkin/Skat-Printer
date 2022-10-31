573113584a942fea157c7973b7799aaa392e4ec7
Mark Tamarov <googlcraftlol@gmail.com>
2022-10-31 23:57:33 +0300

    fix: print functions, created order document

---
+++ scripts/order.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  document.getElementById('orderNumber').innerHTML = params.orderNumber
+  document.getElementById('addressFrom').innerHTML = params.addressFrom
+  document.getElementById('addressTo').innerHTML = params.addressTo
+  document.getElementById('carClass').innerHTML = params.carClass
+  document.getElementById('paymentMethod').innerHTML = params.paymentMethod
+  document.getElementById('options').innerHTML = params.options
+  document.getElementById('price').innerHTML = params.price
+  document.getElementById('car').innerHTML = params.car
+
+  window.print();
+  setTimeout(() => {
+    window.print()
+    setTimeout(() => {window.close()}, 1)
+  }, 1)
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -33,8 +33,7 @@ async function injectScript() {
   
   if (modalData.status === 'success') {
     setUiStatus('Подготовка к печати', 'green');
-    const [{result: printData}] = await executeScript(tab.id, generatePrintImage, [modalData]);
-    setUiStatus(printData.status, 'green');
+    chrome.tabs.create({ url: `order.html${objectToUrl(modalData)}`});
   } else {
     setUiStatus('Окно с заказом не найдено', 'red');
   }
@@ -43,6 +42,17 @@ async function injectScript() {
 const executeScript = (tabId, func, args) => new Promise(resolve => {
   chrome.scripting.executeScript({ target: { tabId }, func, args }, resolve)
 });
+
+function objectToUrl(obj) {
+  let k = Object.keys(obj);
+  let v = Object.values(obj);
+
+  return k.map((el, index) => (
+    index == 0 
+    ? el = `?${el}=${v[index]}`
+    : `&${el}=${v[index]}`
+  )).join('');
+}
 
 function ParseModalData() {
   function sortTags(tagsHtmlCollection, isPaymentMethod) {
@@ -93,74 +103,4 @@ function ParseModalData() {
     }
   }
   return {status: 'error'}
-};
-
-function generatePrintImage(modalData) {
-  let win = window.open('about:blank', "_new");
-  win.document.open();
-  win.document.write(`
-    <html>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet"> 
-        <style type="text/css">
-          h1, h2, h3, h4, h5, p, pre, span {
-            font-family: 'Roboto', sans-serif;
-          }
-          .img_center {
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            margin-bottom: 5px;
-          }
-          #root {
-            display: flex;
-            justify-content: center;
-            flex-direction: column;
-            margin-left: 20px;
-            margin-right: 20px;
-          }
-          #root p {
-            font-weight: 300;
-            margin-top: 4px;
-            margin-bottom: 4px;
-          }
-          .footer {
-            font-weight: 300;
-            text-align: center;
-          }
-          @page {
-            margin: 0;
-          }
-          @media print {
-            html, body {
-              width: 72.1mm;
-              height: 210mm;
-            }
-          }
-        </style>
-      </head>
-      <body onload="window.print()" onafterprint="window.close()">
-        <div id="root">
-          <img class="img_center" src="https://taxipulkovo.com/assets/images/logo.png" />
-          <p><b>${modalData.orderNumber}</b></p>
-          <p>Откуда: <b>${modalData.addressFrom}</b></p>
-          <p>Куда: <b>${modalData.addressTo}</b></p>
-          <p>Класс: <b>${modalData.carClass}</b></p>
-          <p>Способ оплаты: <b>${modalData.paymentMethod}</b></p>
-          <p>Доп. услуги: <b>${modalData.options}</b></p>
-          <p>Стоимость: <b>${modalData.price}</b></p>
-          <p>№ а/м такси: <b>${modalData.car}</b></p>
-          <p class="footer">Данный бланк заказа не является платежным документом</p>
-          <p class="footer">ООО "Управляющая компания<br/>Такси Пулково""</p>
-          <p class="footer">8(812)677-86-35</p>
-          <p class="footer">info@taxipulkovo.com</p>
-          <p class="footer">Сохраняйте бланк заказа до конца поездки!</p>
-        </div>
-      </body>
-    </html>
-  `);
-  win.document.close();
-  return {status: "Отправлено на печать"}
 };
